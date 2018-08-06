c264f09d75c18f865cca99a41a66fe5795a6c6c3
Shuvankar Sarkar <sonu041@gmail.com>
2018-08-06 09:20:34 +0530

    Initial commit

---
+++ app/src/main/java/com/shuvankar/tencoquiz/QuizDbHelper.java
@@ -0,0 +1,93 @@
+package com.shuvankar.tencoquiz;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.shuvankar.tencoquiz.QuizContract.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shuvankar on 05/08/18.
+ */
+
+public class QuizDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "TencoQuiz.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private SQLiteDatabase db;
+    public QuizDbHelper(Context context) {
+        super(context, DATABASE_NAME, null , DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        this.db = db;
+        final String SQL_CREATE_QUESTION_TABLE = "CREATE TABLE " +
+                QuestionsTable.TABLE_NAME + " ( " +
+                QuestionsTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                QuestionsTable.COLUMN_QUESTION + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION1 + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION2 + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION3 + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION4 + " TEXT, " +
+                QuestionsTable.COLUMN_ANSWER_NR + " INTEGER " +
+                ")";
+        db.execSQL(SQL_CREATE_QUESTION_TABLE);
+        fillQuestionsTable();
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + QuestionsTable.TABLE_NAME);
+        onCreate(db);
+
+    }
+    private void fillQuestionsTable() {
+        Question q1 = new Question("A is correct", "A", "B", "C", "D", 1);
+        addQuestion(q1);
+        Question q2 = new Question("C is correct", "A", "B", "C", "D", 3);
+        addQuestion(q2);
+        Question q3 = new Question("B is correct", "A", "B", "C", "D", 2);
+        addQuestion(q3);
+        Question q4 = new Question("A is correct", "A", "B", "C", "D", 1);
+        addQuestion(q4);
+        Question q5 = new Question("D is correct", "A", "B", "C", "D", 4);
+        addQuestion(q5);
+    }
+
+    private void addQuestion(Question question) {
+        ContentValues cv = new ContentValues();
+        cv.put(QuestionsTable.COLUMN_QUESTION, question.getQustion());
+        cv.put(QuestionsTable.COLUMN_OPTION1, question.getOption1());
+        cv.put(QuestionsTable.COLUMN_OPTION2, question.getOption2());
+        cv.put(QuestionsTable.COLUMN_OPTION3, question.getOption3());
+        cv.put(QuestionsTable.COLUMN_OPTION4, question.getOption4());
+        cv.put(QuestionsTable.COLUMN_ANSWER_NR, question.getAnswerNr());
+        db.insert(QuestionsTable.TABLE_NAME, null, cv);
+    }
+
+    public List<Question> getAllQuestion() {
+        List<Question> questionList = new ArrayList<>();
+        db = getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM "+QuestionsTable.TABLE_NAME, null);
+        if(c.moveToFirst()) {
+            do {
+                Question question = new Question();
+                question.setQustion(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_QUESTION)));
+                question.setOption1(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION1)));
+                question.setOption2(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION2)));
+                question.setOption3(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION3)));
+                question.setOption4(c.getString(c.getColumnIndex(QuestionsTable.COLUMN_OPTION4)));
+                question.setAnswerNr(c.getInt(c.getColumnIndex(QuestionsTable.COLUMN_ANSWER_NR)));
+                questionList.add(question);
+            } while (c.moveToNext());
+        }
+        c.close();
+        return questionList;
+    }
+}
